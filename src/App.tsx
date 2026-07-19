31fe932c4f804b1065b055b6dd8d5c0ccc6550ea
Lakshya Saharan <lakshyasaharan@gmail.com>
2026-07-18 22:47:50 -0400

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 
 function App() {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="min-h-screen">
+            <div className="min-h-screen max-w-5xl mx-auto px-4 flex flex-col gap-5 items-center">
+                <h1 className="text-bold text-6xl text-center my-8">Crewmates</h1>
+                <button className="bg-gray-800 text-white px-4 py-2 rounded-md cursor-pointer">Gallary</button>
+                <button className="bg-gray-800 text-white px-4 py-2 rounded-md cursor-pointer">Create Crewmate</button>
+            </div>
         </div>
     )
 }
